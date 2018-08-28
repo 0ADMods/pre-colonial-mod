4a2a8630b383a2dc143b440aa0775cdb395cf0bb
Saul Rodriguez <universosaul@hotmail.com>
2018-08-27 21:49:48 -0600

    add tlaxcaltecs buildings
    -Barracks
    -Civil Centre
    -Dock
    - 3 houses
    -Outpost for the new civis of the mod
    -Farmstead
    -Storehouse
    - Tupi Civic Centre

--- gui/common/music_precolonial.js
+++ gui/common/music_precolonial.js
@@ -1,11 +1,11 @@
 Music.prototype.resetTracks = function()
-{
-	this.tracks = {
-		"MENU": [""],
-		"PEACE": [],
-		"BATTLE": [""],
-		"VICTORY": [""],
-		"DEFEAT": [""],
-		"CUSTOM": []
-	};
-};
+ {
+ 	this.tracks = {
+		"MENU": [],
+		"PEACE": ["Chee_Zee_Jungle","Cloud_People.ogg","Old New World.ogg","Tradition.ogg"],
+		"BATTLE": ["Taiko_1.ogg", "Taiko_2.ogg"],
+		"VICTORY": ["You_are_Victorious!.ogg"],
+		"DEFEAT": [],
+		"CUSTOM": [""]
+ 	};
+ };
