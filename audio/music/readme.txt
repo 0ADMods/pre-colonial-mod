6be3644689ee804bb2217a517fd86bb736e7e1d2
Saúl Rodríguez Martínez <universosaul@hotmail.com>
2020-04-23 22:35:35 -0600

    changes: delete zapotec soundtracks

--- audio/music/readme.txt
+++
@@ -1,1 +0,0 @@
-I use the zapotec music of terra magna mod only as placeholder :)
--- gui/common/music_precolonial.js
+++ gui/common/music_precolonial.js
@@ -1,8 +1,8 @@
 Music.prototype.resetTracks = function()
  {
  	this.tracks = {
-		"MENU": ["Old_New_World.ogg"],
-		"PEACE": ["Chee_Zee_Jungle.ogg","Cloud_People.ogg","Tradition.ogg"],
+		"MENU": [],
+		"PEACE": [],
 		"BATTLE": ["Taiko_1.ogg", "Taiko_2.ogg"],
 		"VICTORY": ["You_are_Victorious!.ogg"],
 		"DEFEAT": [],
