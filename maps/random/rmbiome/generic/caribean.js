2deb1d4360eff3e7ad813014548a46efae82a988
Welton d.S. Lopes <weltonlopesmetal@gmail.com.br>
2021-04-09 00:09:30 -0300

    Algunos errores arreglados, jugables. algunos archivos guardados para futuras correcciones

---
+++ maps/random/rmbiome/generic/caribean.js
@@ -0,0 +1,31 @@
+function setupBiome_caribean()
+{
+	[g_Gaia.tree1, g_Gaia.tree2] = pickRandom([
+		[
+			"gaia/tree/date_palm",
+			"gaia/tree/palm_tropical"
+		],
+		[
+			"gaia/tree/palm_areca",
+			"gaia/tree/date_palm"
+		],
+		[
+			"gaia/tree/palm_areca",
+			"gaia/tree/palm_tropic"
+		]]);
+
+	g_Gaia.tree3 = pickRandom([
+		"gaia/fruit/date",
+		"gaia/fruit/berry_05"
+	]);
+
+	[g_Gaia.tree4, g_Gaia.tree5] = new Array(2).fill(pickRandom([
+		"gaia/tree/banyan",
+		"gaia/tree/bush_tropic"
+	]));
+
+	g_Gaia.fruitBush = pickRandom([
+		"gaia/fruit/berry_05",
+		"gaia/fruit/date"
+	]);
+}
---
+++ maps/random/rmbiome/generic/highland.js
@@ -0,0 +1,7 @@
+function setupBiome_highland()
+{
+	g_Gaia.mainHuntableAnimal = pickRandom([
+		"gaia/fauna_deer_white_tailed",
+		"gaia/fauna_turkey"
+	]);
+}
